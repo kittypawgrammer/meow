eb0188a56d1c001a2947258b4d8b6144ef59318c
Priyanka Kanwal <priyankanwal967@gmail.com>
2025-06-20 13:27:01 +0530

    Add: repo & Service class

--- meow/src/main/java/com/example/CatFact/Repository/CatFactRepository.java
+++ meow/src/main/java/com/example/CatFact/Repository/CatFactRepository.java
@@ -2,8 +2,11 @@ package com.example.CatFact.Repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.CatFact.Entity.CatFacts;
 
+@Repository
 public interface CatFactRepository extends JpaRepository<CatFacts, Long> {
     Optional<CatFacts> findByText(String text);
 }
--- meow/src/main/java/com/example/CatFact/Service/CatfactService.java
+++ meow/src/main/java/com/example/CatFact/Service/CatfactService.java
@@ -1,5 +1,10 @@
 package com.example.CatFact.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CatfactService {
+
+    
 
 }
